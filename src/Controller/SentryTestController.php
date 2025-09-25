67c84e342c1fcf55ccdd115a3f437355e2d42a77
Abboud Mechehoud <abboud.mechehoud@gmail.com>
2025-09-25 09:58:20 +0400

    feat(p0-5.2): add Sentry (dsn from env) + Monolog handler + /_sentry-test route

---
+++ src/Controller/SentryTestController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class SentryTestController extends AbstractController
+{
+    public function __construct(private LoggerInterface $logger) {}
+
+    #[Route('/_sentry-test', name: 'sentry_test')]
+    public function testLog(): never
+    {
+        // 1) Un log ERROR doit partir vers Sentry via Monolog handler "sentry"
+        $this->logger->error('My custom logged error.', ['some' => 'Context Data']);
+
+        // 2) Une exception non-catchée doit être capturée aussi
+        throw new \RuntimeException('Example exception.');
+    }
+}
