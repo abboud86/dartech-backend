ef1b90d3b94e9fb28f9936e5dd7f42e7da42c529
Abboud Mechehoud <abboud.mechehoud@gmail.com>
2025-09-26 14:26:49 +0400

    chore(p0-7.8): add PHP-CS-Fixer + PHPStan (CI cs/phpstan) & fix return types for phpstan

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,19 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder())
+    ->in([__DIR__.'/src', __DIR__.'/tests'])
+    ->ignoreDotFiles(true)
+    ->ignoreVCS(true);
+
+$config = new PhpCsFixer\Config();
+return $config
+    ->setRiskyAllowed(true)
+    ->setCacheFile(__DIR__.'/.php-cs-fixer.cache')
+    ->setRules([
+        '@Symfony' => true,
+        '@Symfony:risky' => true,
+        'phpdoc_align' => false,
+        'native_function_invocation' => false,
+        'declare_strict_types' => false,
+    ])
+    ->setFinder($finder);
