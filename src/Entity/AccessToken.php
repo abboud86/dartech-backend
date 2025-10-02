a797445e69267926a83588e8e867f1c1ed2c994e
Abboud Mechehoud <abboud.mechehoud@gmail.com>
2025-10-02 17:23:36 +0400

    fix(auth): AccessToken id switched to ULID (UlidType + doctrine.ulid_generator)

---
+++ src/Entity/AccessToken.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AccessTokenRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Types\UlidType;
+use Symfony\Component\Uid\Ulid;
+
+#[ORM\Entity(repositoryClass: AccessTokenRepository::class)]
+class AccessToken
+{
+    #[ORM\Id]
+    #[ORM\Column(type: UlidType::NAME, unique: true)]
+    #[ORM\GeneratedValue(strategy: 'CUSTOM')]
+    #[ORM\CustomIdGenerator(class: 'doctrine.ulid_generator')]
+    private ?Ulid $id = null;
+
+    public function getId(): ?Ulid
+    {
+        return $this->id;
+    }
+}
