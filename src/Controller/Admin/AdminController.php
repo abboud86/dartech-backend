e630e47cd2aa22daabf2ab032c43fb802372468a
Abboud Mechehoud <abboud.mechehoud@gmail.com>
2025-11-16 21:06:28 +0400

    feat(p4-01.1): init admin module

---
+++ src/Controller/Admin/AdminController.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class AdminController extends AbstractController
+{
+    #[Route('/admin', name: 'admin_dashboard', methods: ['GET'])]
+    public function index(): Response
+    {
+        return $this->render('admin/dashboard.html.twig', [
+            'section' => 'dashboard',
+        ]);
+    }
+}
